833dd319527c4626795b0be3e2e96de574b35fb4
@vitoralves <01vitoralves@gmail.com>
2021-07-18 15:14:08 -0300

    atualizacao

--- src/components/ProfileRelationsBox/index.js
+++ src/components/ProfileRelationsBox/index.js
@@ -46,7 +46,7 @@ export function CommunityBox(props) {
                             return (
                                 <li key={itemAtual.id}>
 
-                                    {/* <a
+                                    <a
                                         onClick={() => {
                                             router.push({
                                                 pathname: '/community',
@@ -56,11 +56,8 @@ export function CommunityBox(props) {
                                     >
                                         <img src={itemAtual.imageUrl} />
                                         <span>{itemAtual.title}</span>
-                                    </a> */}
-                                    <a>
-                                        <img src={itemAtual.imageUrl} />
-                                        <span>{itemAtual.title}</span>
                                     </a>
+
 
                                 </li>
                             )
